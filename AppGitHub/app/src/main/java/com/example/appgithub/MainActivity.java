cc21a100d39ee9bb7e17bd103a90ce65197dfbc4
unknown <joaovitormonsores@hotmail.com>
2022-11-10 17:16:34 -0300

    implements dark-mode theme and  switch buttom

--- AppGitHub/app/src/main/java/com/example/appgithub/MainActivity.java
+++ AppGitHub/app/src/main/java/com/example/appgithub/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.appgithub;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -19,15 +23,55 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView nameView, userView, orgView, bioView, fwsView, fwgView, repView;
+    private TextView nameView, userView, orgView, bioView, fwsView, fwgView, repView, modeText;
     private RequestQueue queue;
     private ImageView avatar;
     private Context context;
+    private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkMode);
+
+        } else {
+            setTheme(R.style.Theme_AppGitHub);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aSwitch = findViewById(R.id.mode);
+        modeText = findViewById(R.id.modeText);
+
+        if (aSwitch.isActivated()) {
+            modeText.setText("Dark Mode");
+
+        } else {
+            modeText.setText("Light Mode");
+
+        }
+
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            aSwitch.setChecked(true);
+            modeText.setText("Dark Mode");
+        }
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    modeText.setText("Dark Mode");
+
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    modeText.setText("Light Mode");
+
+                }
+            }
+        });
         components();
         requestUser();
     }
